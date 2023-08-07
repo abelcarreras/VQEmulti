6879225e9e6379f194f600f38bd1e60f5976966d
Abel Carreras <abelcarreras83@gmail.com>
2023-08-07 10:51:43 +0200

    Add ucc_exact energy function, changed exact functions docstrings

--- vqemulti/energy/exact.py
+++ vqemulti/energy/exact.py
@@ -8,7 +8,7 @@ def exact_vqe_energy(coefficients, ansatz, hf_reference_fock, hamiltonian):
     """
     Calculates the energy of the state prepared by applying an ansatz (of the
     type of the Adapt VQE protocol) to a reference state.
-    Uses instances of scipy.sparse.csc_matrix for efficiency.
+    Applies Trotter approach (n=1) -> psi> = prod(e^(theta_i*T_i)) |0>
 
     :param coefficients: the list of coefficients of the ansatz operators
     :param ansatz: ansatz expressed in qubit/fermion operators
@@ -47,6 +47,7 @@ def exact_vqe_energy_gradient(coefficients, ansatz, hf_reference_fock, hamiltoni
     """
     Calculates the gradient of the energy with respect to the coefficients.
     To be used in the exact energy minimization function
+    Assumes Trotter approach (n=1) -> psi> = prod(e^(theta_i*T_i)) |0>
 
     :param coefficients: the list of coefficients of the ansatz operators
     :param ansatz: ansatz expressed in qubit/fermion operators
@@ -95,3 +96,40 @@ def exact_vqe_energy_gradient(coefficients, ansatz, hf_reference_fock, hamiltoni
     recurse(hbra, ket, 0)
 
     return gradient
+
+
+def exact_vqe_energy_no_trotter(coefficients, ansatz, hf_reference_fock, hamiltonian):
+    """
+    Calculates the energy of the state prepared by applying an ansatz (of the
+    type of the Adapt VQE protocol) to a reference state.
+    Uses Psi> =  e^sum(theta_i * T_i) |0>
+
+    :param coefficients: the list of coefficients of the ansatz operators
+    :param ansatz: ansatz expressed in qubit/fermion operators
+    :param hf_reference_fock: HF reference in Fock space vector
+    :param hamiltonian: Hamiltonian in FermionOperator/InteractionOperator
+    :return: exact energy
+    """
+
+    # Transform Hamiltonian to matrix representation
+    sparse_hamiltonian = get_sparse_operator(hamiltonian)
+
+    # Find the number of qubits of the system (2**n_qubit = dimension)
+    n_qubit = count_qubits(hamiltonian)
+
+    # Transform reference vector into a Compressed Sparse Column matrix
+    ket = get_sparse_ket_from_fock(hf_reference_fock)
+
+    # Get total exponent operator list
+    exponent = scipy.sparse.csr_array((2**n_qubit, 2**n_qubit), dtype=float)
+    for coefficient, operator in zip(coefficients, ansatz):
+        exponent += coefficient * get_sparse_operator(operator, n_qubit)
+
+    # Apply operator to ket
+    ket = scipy.sparse.linalg.expm_multiply(exponent, ket)
+
+    # Get the corresponding bra and calculate the energy: |<bra| H |ket>|
+    bra = ket.transpose().conj()
+    energy = np.sum(bra * sparse_hamiltonian * ket).real
+
+    return energy

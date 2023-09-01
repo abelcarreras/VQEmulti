26ca4bd23d2003e6abff8eb65a720d8e84050ec1
Abel Carreras <abelcarreras83@gmail.com>
2023-09-01 12:31:47 +0200

    update to more efficient quantum fidelity calculation

--- vqemulti/density.py
+++ vqemulti/density.py
@@ -55,6 +55,93 @@ def get_density_matrix(coefficients, ansatz, hf_reference_fock, n_orbitals, froz
 
     return density_matrix_alpha + density_matrix_beta
 
+def get_second_order_density_matrix(coefficients, ansatz, hf_reference_fock, n_orbitals, frozen_core=0):
+    """
+    Calculates the 2nd order density matrix in molecular orbitals basis
+
+    :param coefficients: the list of coefficients of the ansatz operators
+    :param ansatz: ansatz expressed in qubit/fermion operators
+    :param hf_reference_fock: HF reference in Fock space vector
+    :param n_orbitals: number of molecular orbitals
+    :return: exact energy
+    """
+
+    # get number of qubits
+    n_orbitals = n_orbitals
+    n_qubit = (n_orbitals - frozen_core) * 2
+
+    # Transform reference vector into a Compressed Sparse Column matrix
+    ket = get_sparse_ket_from_fock(hf_reference_fock)
+
+    # Get total exponent operator list
+    exponent = scipy.sparse.csr_array((2 ** n_qubit, 2 ** n_qubit), dtype=float)
+    for coefficient, operator in zip(coefficients, ansatz):
+        exponent += coefficient * get_sparse_operator(operator, n_qubit)
+
+    # Apply operator to ket
+    ket = scipy.sparse.linalg.expm_multiply(exponent, ket)
+
+    # Get the corresponding bra and calculate the energy: |<bra| H |ket>|
+    bra = ket.transpose().conj()
+
+    # initialize density matrices
+    density_matrix_alpha = np.zeros((n_orbitals, n_orbitals, n_orbitals, n_orbitals))
+    density_matrix_beta = np.zeros((n_orbitals, n_orbitals, n_orbitals, n_orbitals))
+    density_matrix_cross = np.zeros((n_orbitals, n_orbitals, n_orbitals, n_orbitals))
+
+    for i in range(frozen_core):
+        density_matrix_alpha[i, i, i, i] = 1
+        density_matrix_beta[i, i, i, i] = 1
+
+    # build density matrix
+    for i1 in range(n_orbitals - frozen_core):
+        for i2 in range(n_orbitals - frozen_core):
+
+            for j1 in range(n_orbitals - frozen_core):
+                for j2 in range(n_orbitals - frozen_core):
+
+                    # alpha
+                    density_operator = FermionOperator(((j1*2, 1), (j2*2, 1), (i2*2, 0), (i1*2, 0)))
+                    #print(density_operator)
+                    sparse_operator = get_sparse_operator(density_operator, n_qubit)
+                    density_matrix_alpha[i1+frozen_core, i2+frozen_core,
+                                         j2+frozen_core, j1+frozen_core] = np.sum(bra * sparse_operator * ket).real
+
+                    # beta
+                    density_operator = FermionOperator(((j1*2+1, 1), (j2*2+1, 1), (i2*2+1, 0), (i1*2+1, 0)))
+                    sparse_operator = get_sparse_operator(density_operator, n_qubit)
+                    density_matrix_beta[i1+frozen_core, i2+frozen_core,
+                                        j2+frozen_core, j1+frozen_core] = np.sum(bra * sparse_operator * ket).real
+
+                    # cross 1
+                    density_operator = FermionOperator(((j1*2, 1), (j2*2+1, 1), (i2*2, 0), (i1*2+1, 0)))
+                    sparse_operator = get_sparse_operator(density_operator, n_qubit)
+                    density_matrix_cross[i1+frozen_core, i2+frozen_core,
+                                         j2+frozen_core, j1+frozen_core] -= np.sum(bra * sparse_operator * ket).real
+                    #print(np.sum(bra * sparse_operator * ket).real)
+
+                    # cross 2
+                    density_operator = FermionOperator(((j1*2+1, 1), (j2*2, 1), (i2*2+1, 0), (i1*2, 0)))
+                    sparse_operator = get_sparse_operator(density_operator, n_qubit)
+                    density_matrix_cross[i1+frozen_core, i2+frozen_core,
+                                         j2+frozen_core, j1+frozen_core] -= np.sum(bra * sparse_operator * ket).real
+                    #print(np.sum(bra * sparse_operator * ket).real)
+
+                   # cross 3
+                    density_operator = FermionOperator(((j1*2+1, 1), (j2*2, 1), (i2*2, 0), (i1*2+1, 0)))
+                    sparse_operator = get_sparse_operator(density_operator, n_qubit)
+                    density_matrix_cross[i1+frozen_core, i2+frozen_core,
+                                         j2+frozen_core, j1+frozen_core] += np.sum(bra * sparse_operator * ket).real
+                    #print(np.sum(bra * sparse_operator * ket).real)
+
+                    # cross 4
+                    density_operator = FermionOperator(((j1*2, 1), (j2*2+1, 1), (i2*2+1, 0), (i1*2, 0)))
+                    sparse_operator = get_sparse_operator(density_operator, n_qubit)
+                    density_matrix_cross[i1+frozen_core, i2+frozen_core,
+                                         j2+frozen_core, j1+frozen_core] += np.sum(bra * sparse_operator * ket).real
+                    #print(np.sum(bra * sparse_operator * ket).real)
+
+    return 0.5 * (3*density_matrix_alpha + 3*density_matrix_beta + density_matrix_cross)
 
 def density_fidelity(density_ref, density_vqe):
     """
@@ -76,12 +163,15 @@ def density_fidelity(density_ref, density_vqe):
     n_pad = len(density_ref) - len(density_vqe)
     density_vqe = np.pad(density_vqe, (0, n_pad), 'constant')
 
-    sqrt_ref = scipy.linalg.sqrtm(density_ref)
     sqrt_vqe = scipy.linalg.sqrtm(density_vqe)
 
-    dens = np.dot(sqrt_ref, sqrt_vqe)
+    dens = np.dot(np.dot(sqrt_vqe, density_ref), sqrt_vqe)
+    trace = np.trace(scipy.linalg.sqrtm(dens))
 
-    trace = np.trace(scipy.linalg.sqrtm(np.dot(dens.T, dens)))
+    # alternative
+    # sqrt_ref = scipy.linalg.sqrtm(density_ref)
+    # dens = np.dot(sqrt_ref, sqrt_vqe)
+    # trace = np.trace(scipy.linalg.sqrtm(np.dot(dens.T, dens)))
 
     return np.absolute(trace)**2
 

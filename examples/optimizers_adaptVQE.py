c00e3a284314b707c86ee33e362c432f5a933701
Abel Carreras <abelcarreras83@gmail.com>
2024-04-02 11:17:56 +0200

    add custom optimizers (adam, sgd & rmsprop) and example

--- examples/optimizers_adaptVQE.py
+++ examples/optimizers_adaptVQE.py
@@ -9,21 +9,44 @@ from vqemulti.simulators.qiskit_simulator import QiskitSimulator as Simulator
 from openfermion import MolecularData
 from openfermionpyscf import run_pyscf
 import matplotlib.pyplot as plt
+from vqemulti.optimizers import adam, rmsprop, sgd
+
+
+def shot_model_1(dict_parameters):
+    import numpy as np
+
+    s_prec = 1
+    energy_threshold = dict_parameters['precision']/s_prec
+    variance = dict_parameters['variance']
+    n_coefficients = dict_parameters['n_coefficients']
+    n_coefficients = 1 if n_coefficients < 1 else n_coefficients
+
+    n_shots_ave = variance * n_coefficients ** 2 * np.pi ** 2 / (100 * energy_threshold ** 2)
+    n_shots_dev = variance * n_coefficients ** (2 / 3) / (energy_threshold ** 2)
+
+    n_shots = int(max(n_shots_ave, n_shots_dev))
+
+    tolerance = energy_threshold* min(1/n_coefficients**(1/3), 10 /(np.pi * n_coefficients))
+
+    return tolerance, n_shots
 
 
 # general configuration
 Configuration().verbose = True
 
 # molecule definition
-he2_molecule = MolecularData(geometry=[['He', [0, 0, 0]],
-                                       ['He', [0, 0, 1.0]]],
-                             basis='6-31g',
-                             multiplicity=1,
-                             charge=0,
-                             description='He2')
+distance = 1.0
+h4_molecule = MolecularData(geometry=[['H', [0, 0, 0]],
+                                      ['H', [distance, 0, 0]],
+                                      ['H', [0, distance, 0]],
+                                      ['H', [distance, distance, 0]]],
+                            basis='6-31g',
+                            multiplicity=1,
+                            charge=0,
+                            description='H4')
 
 # run classical calculation
-molecule = run_pyscf(he2_molecule, run_fci=True)
+molecule = run_pyscf(h4_molecule, run_fci=True)
 
 # get properties from classical SCF calculation
 n_electrons = molecule.n_electrons
@@ -38,11 +61,17 @@ pool = get_pool_singlet_sd(n_electrons=n_electrons, n_orbitals=n_orbitals)
 hf_reference_fock = get_hf_reference_in_fock_space(n_electrons, hamiltonian.n_qubits)
 
 # define simulator
-simulator = Simulator(trotter=False, test_only=True, hamiltonian_grouping=True, shots=250)
+simulator = Simulator(trotter=False, test_only=True, hamiltonian_grouping=True) #, shots=250)
+simulator.set_shots_model(shot_model_1)
 
 # define optimizer (check: https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html)
 opt_bfgs = OptimizerParams(method='BFGS', options={'gtol': 1e-2})
 opt_cobyla = OptimizerParams(method='COBYLA', options={'rhobeg': 0.1})
+
+# define custom optimizers
+opt_adam = OptimizerParams(method=adam, options={'learning_rate': 0.01, 'gtol': 1e-2})
+opt_rmsprop = OptimizerParams(method=rmsprop, options={'learning_rate': 0.01, 'gtol': 1e-2})
+opt_sgd = OptimizerParams(method=sgd, options={'learning_rate': 0.01, 'gtol': 1e-2})
 
 try:
     # run adaptVQE
@@ -54,7 +83,7 @@ try:
                       energy_threshold=1e-2,
                       opt_qubits=False,  # use fermion operators
                       max_iterations=15,  # maximum number of interations
-                      optimizer_params=opt_cobyla # optimizer parameters
+                      optimizer_params=opt_adam  # optimizer parameters
                       )
 
 except NotConvergedError as e:
--- vqemulti/optimizers.py
+++ vqemulti/optimizers.py
@@ -1,5 +1,6 @@
 from scipy.optimize import OptimizeResult
 from vqemulti.preferences import Configuration
+import numpy as np
 
 
 class OptimizerParams:
@@ -28,14 +29,172 @@ class OptimizerParams:
         return self._options
 
 
-def spsa_minimizer(fun, x0, args=None, **options):
+def disp_message_print(f_val, success, iterations):
+    if success:
+        print('Optimization terminated successfully.')
+    else:
+        print('Optimization terminated unsuccessfully.')
+
+    print('     Current function value:', f_val)
+    print('     Iterations:', iterations)
+    print('     Gradient evaluations:', iterations)
+
+
+def sgd(fun, x0, jac, args=(), **options):
     """
-    wrapper of sps implementation module to works as a custom method for scipy minimizer
+    ``scipy.optimize.minimize`` compatible implementation of stochastic gradient descent with momentum.
+     Adapted from https://gist.github.com/jcmgray/
+
     :param fun: optimization function
     :param x0: initial guess
     :param args: optimization function additional arguments
     :param options: additional options (not used for now)
-    :return:
+    :return: OptimizeResult
+    """
+    # default parameters
+    params = {'learning_rate': 0.01,
+              'mass': 0.9,
+              'maxiter': 1000,
+              'gtol': 1-5}
+
+    params.update(options)
+
+    learning_rate = params['learning_rate']
+    mass = params['mass']
+
+    x = x0
+    velocity = np.zeros_like(x)
+    i = 0
+    success = False
+
+    for i in range(params['maxiter']):
+        g = np.array(jac(x, *args))
+
+        velocity = mass * velocity - (1.0 - mass) * g
+        x = x + learning_rate * velocity
+
+        if abs(np.linalg.norm(g)) < params['gtol']:
+            success = True
+            break
+
+    i += 1
+
+    if options['disp']:
+        disp_message_print(fun(x, *args), success, i)
+
+    return OptimizeResult(x=x, fun=fun(x, *args), jac=g, nit=i, nfev=i, success=True)
+
+
+def rmsprop(fun, x0, jac, args=(), **options):
+    """
+    ``scipy.optimize.minimize`` compatible implementation of root mean squared prop: See Adagrad paper for details.
+     Adapted from https://gist.github.com/jcmgray/
+
+    :param fun: optimization function
+    :param x0: initial guess
+    :param args: optimization function additional arguments
+    :param options: additional options (not used for now)
+    :return: OptimizeResult
+    """
+
+    # default parameters
+    params = {'learning_rate': 0.01,
+              'gamma': 0.9,
+              'eps': 1e-6,
+              'maxiter': 1000,
+              'gtol': 1-5}
+
+    params.update(options)
+
+    learning_rate = params['learning_rate']
+    gamma = params['gamma']
+    eps = params['eps']
+
+    x = x0
+    avg_sq_grad = np.ones_like(x)
+    i = 0
+    success = False
+
+    for i in range(params['maxiter']):
+        g = np.array(jac(x, *args))
+
+        avg_sq_grad = avg_sq_grad * gamma + g**2 * (1 - gamma)
+        x = x - learning_rate * g / (np.sqrt(avg_sq_grad) + eps)
+
+        if abs(np.linalg.norm(g)) < params['gtol']:
+            success = True
+            break
+
+    i += 1
+
+    if options['disp']:
+        disp_message_print(fun(x, *args), success, i)
+
+    return OptimizeResult(x=x, fun=fun(x, *args), jac=g, nit=i, nfev=i, success=True)
+
+
+def adam(fun, x0, jac, args=(), **options):
+    """
+    ``scipy.optimize.minimize`` compatible implementation of ADAM - [http://arxiv.org/pdf/1412.6980.pdf].
+     Adapted from https://gist.github.com/jcmgray/
+
+    :param fun: optimization function
+    :param x0: initial guess
+    :param args: optimization function additional arguments
+    :param options: additional options (not used for now)
+    :return: OptimizeResult
+    """
+    # default parameters
+    params = {'learning_rate': 0.01,
+              'beta1': 0.9,
+              'beta2': 0.999,
+              'eps': 1e-6,
+              'maxiter': 1000,
+              'gtol': 1-5}
+
+    params.update(options)
+
+    learning_rate = params['learning_rate']
+    beta1 = params['beta1']
+    beta2 = params['beta2']
+    eps = params['eps']
+
+    x = x0
+    m = np.zeros_like(x)
+    v = np.zeros_like(x)
+    i = 0
+    success = False
+
+    for i in range(params['maxiter']):
+        g = np.array(jac(x, *args))
+
+        m = (1 - beta1) * g + beta1 * m  # first  moment estimate.
+        v = (1 - beta2) * (g**2) + beta2 * v  # second moment estimate.
+        mhat = m / (1 - beta1**(i + 1))  # bias correction.
+        vhat = v / (1 - beta2**(i + 1))
+        x = x - learning_rate * mhat / (np.sqrt(vhat) + eps)
+
+        if abs(np.linalg.norm(g)) < params['gtol']:
+            success = True
+            break
+
+    i += 1
+
+    if options['disp']:
+        disp_message_print(fun(x, *args), success, i)
+
+    return OptimizeResult(x=x, fun=fun(x, *args), jac=g, nit=i, nfev=i, success=success)
+
+
+def spsa_minimizer(fun, x0, args=(), **options):
+    """
+    wrapper of sps implementation module to works as a custom method for scipy minimizer
+
+    :param fun: optimization function
+    :param x0: initial guess
+    :param args: optimization function additional arguments
+    :param options: additional options (not used for now)
+    :return: OptimizeResult
     """
     import spsa
     global nfev

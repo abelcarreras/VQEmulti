2fe63f963eaecdf634530834ca24579510d607aa
NoniaVS <noniavs@gmail.com>
2024-07-11 20:54:08 +0200

    update genetic_adapt.py to initialize outside adapt function create example scan_GeneticVQE.py

--- vqemulti/method/genetic_adapt.py
+++ vqemulti/method/genetic_adapt.py
@@ -9,25 +9,18 @@ import numpy as np
 
 class GeneticAdapt(Method):
 
-    def __init__(self, energy_threshold, gradient_threshold, operator_update_number,
-                 operator_update_max_grad, coeff_tolerance, diff_threshold,
-                 gradient_simulator, hf_reference_fock, hamiltonian,
-                 operators_pool, variance, iterations, energy_simulator, beta, *args):
-        super().__init__(hf_reference_fock, hamiltonian,
-                  operators_pool, variance, iterations, energy_simulator)
-        self.energy_threshold = energy_threshold
+    def __init__(self, gradient_threshold, diff_threshold, coeff_tolerance,
+                 gradient_simulator, beta):
         self.gradient_threshold = gradient_threshold
-        self.operator_update_number = 1
-        self.operator_update_max_grad = operator_update_max_grad
-        self.gradient_simulator = gradient_simulator
         self.diff_threshold = diff_threshold
         self.coeff_tolerance = coeff_tolerance
+        self.gradient_simulator = gradient_simulator
         self.beta = beta
 
         # Convergence criteria definition for this method
         self.criteria_list = [zero_valued_coefficient_adaptvanilla]
         self.params_convergence = {'coeff_tolerance': self.coeff_tolerance, 'diff_threshold': self.diff_threshold,
-                                   'operator_update_number': self.operator_update_number}
+                                   }
 
 
 
@@ -92,16 +85,7 @@ class GeneticAdapt(Method):
                 raise Converged(message='Converge archived due to gradient norm threshold')
 
             # primary selection of operators
-            max_indices = np.argsort(gradient_vector)[-self.operator_update_number:][::-1]
-
-            # refine selection to ensure all operators are relevant
-            while True:
-                max_gradients = np.array(gradient_vector)[max_indices]
-                max_dev = np.max(np.std(max_gradients))
-                if max_dev / np.max(max_gradients) > self.operator_update_max_grad:
-                    max_indices = max_indices[:-1]
-                else:
-                    break
+            max_indices = np.argsort(gradient_vector)[-1:][::-1]
 
             # get gradients/operators update list
             max_gradients = np.array(gradient_vector)[max_indices]

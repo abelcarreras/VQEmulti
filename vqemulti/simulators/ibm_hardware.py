b47a40cc40a655cf4ed0c4b961d34479356ad252
Abel Carreras <abelcarreras83@gmail.com>
2025-05-16 17:19:26 +0200

    update add compatibility to new version of qiskit

--- vqemulti/simulators/ibm_hardware.py
+++ vqemulti/simulators/ibm_hardware.py
@@ -24,7 +24,7 @@ class RHESampler:
         isa_circuit = self._pm.run(circuit)
 
         from qiskit_ibm_runtime import SamplerV2
-        sampler = SamplerV2(session=self._session)
+        sampler = SamplerV2(mode=self._session)
         job = sampler.run([isa_circuit], shots=shots)
         pub_result = job.result()[0]
         # counts_total = pub_result.data.meas.get_counts()
@@ -71,6 +71,8 @@ class RHEstimator:
         if Configuration().verbose > 1:
             print('depth: ', circuit.depth())
             print('isa_depth: ', isa_circuit.depth())
+            print('circuit gate count: ', dict(circuit.count_ops()))
+            print('isa_circuit gate count: ', dict(isa_circuit.count_ops()))
             # print('observable: ', measure_op)
             print('n_observable: ', len(measure_op))
             print('n_chunks: ', n_chunks)

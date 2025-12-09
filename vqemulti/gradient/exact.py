f9cf1274f51727bef3949b75a520be6eba86caa0
Abel Carreras <abelcarreras83@gmail.com>
2025-12-09 17:17:09 +0100

    make gradient exact calculation more robust

--- vqemulti/gradient/exact.py
+++ vqemulti/gradient/exact.py
@@ -106,11 +106,11 @@ def exact_adapt_vqe_energy_gradient(coefficients, ansatz, hf_reference_fock, ham
     :return: gradient vector
     """
 
-    # Transform Hamiltonian to matrix representation
-    sparse_hamiltonian = get_sparse_operator(hamiltonian)
-
     # Find the number of qubits of the system (2**n_qubit = dimension)
-    n_qubit = count_qubits(hamiltonian)
+    n_qubit = len(hf_reference_fock)
+
+    # Transform Hamiltonian to matrix representation
+    sparse_hamiltonian = get_sparse_operator(hamiltonian, n_qubit)
 
     # Transform reference vector into a Compressed Sparse Column matrix
     ket = get_sparse_ket_from_fock(hf_reference_fock)

50511001f02577aa15e0f72b391810aea6543d21
Abel Carreras <abelcarreras83@gmail.com>
2025-12-24 18:13:27 +0100

    update example to use new Ansatz objects

--- examples/scan_GeneticAddVQE.py
+++ examples/scan_GeneticAddVQE.py
@@ -5,6 +5,7 @@ from vqemulti.pool import get_pool_singlet_sd
 from vqemulti.adapt_vqe import adaptVQE
 from vqemulti.errors import NotConvergedError
 from vqemulti.analysis import get_info
+from vqemulti.ansatz.exp_product import ProductExponentialAnsatz
 from openfermion import MolecularData
 from openfermionpyscf import run_pyscf
 import matplotlib.pyplot as plt
@@ -17,16 +18,16 @@ for d in np.linspace(0.3, 3, 5):
 
     # molecule definition
     h2_molecule = MolecularData(geometry=[['H', [0, 0, 0]],
-                                  ['H', [0, 0, d]],
-                                  ['H', [0, 0, 2 * d]],
-                                  ['H', [0, 0, 3 * d]]],
-                                basis='3-21g',
+                                          ['H', [0, 0, d]],
+                                          ['H', [0, 0, 2 * d]],
+                                          ['H', [0, 0, 3 * d]]],
+                                basis='sto-3g',
                                 multiplicity=1,
                                 charge=0,
                                 description='H4')
 
     # run classical calculation
-    molecule = run_pyscf(h2_molecule, run_fci=True)
+    molecule = run_pyscf(h2_molecule, run_fci=True, verbose=True)
 
     # get additional info about electronic structure properties
     # get_info(molecule, check_HF_data=False)
@@ -43,6 +44,9 @@ for d in np.linspace(0.3, 3, 5):
     # Get Hartree Fock reference in Fock space
     hf_reference_fock = get_hf_reference_in_fock_space(n_electrons, hamiltonian.n_qubits)
 
+    # build initial anasatz
+    ansatz = ProductExponentialAnsatz([], [], hf_reference_fock)
+
     from vqemulti.simulators.qiskit_simulator import QiskitSimulator as Simulator
 
     # define simulator paramters
@@ -53,29 +57,27 @@ for d in np.linspace(0.3, 3, 5):
     from vqemulti.method.genetic_add_adapt import Genetic_Add_Adapt
 
     method = Genetic_Add_Adapt(gradient_threshold=1e-6,
-                         diff_threshold=0,
-                         coeff_tolerance=1e-10,
-                         gradient_simulator=None,
-                         beta=6,
-                         alpha=0.001
-                         )
+                               diff_threshold=0,
+                               coeff_tolerance=1e-10,
+                               gradient_simulator=None,
+                               beta=6,
+                               alpha=0.001
+                               )
     # run adaptVQE
     try:
         result = adaptVQE(hamiltonian,     # fermionic hamiltonian
                           operators_pool,  # fermionic operators
-                          hf_reference_fock,
+                          ansatz,
                           energy_threshold=0.0001,
-                          method = method,
-                          max_iterations = 10,
-                          energy_simulator = None,
-                          variance_simulator = None,
-                          reference_dm = None,
-                          optimizer_params = None
+                          method=method,
+                          max_iterations=10,
+                          energy_simulator=None,
+                          reference_dm=None,
+                          optimizer_params=None
                           )
     except NotConvergedError as c:
         print('Not converged :(')
         result = c.results
-
 
     print("Final energy:", result["energy"])
 

867d89a4c85857957c745af1bd4fab137d2ec798
Abel Carreras <abelcarreras83@gmail.com>
2026-07-25 00:10:44 +0200

    add LayoutModel Graph

--- vqemulti/simulators/layout.py
+++ vqemulti/simulators/layout.py
@@ -33,6 +33,178 @@ def get_paths(parelles, N):
     return tots_camins
 
 
+# draw graph
+def draw_graph(G,
+               pos=None,
+               ax=None,
+               node_size=600,
+               node_color="tab:blue",
+               edge_color="black",
+               with_labels=True,
+               labels=None):
+    """
+    Draw a NetworkX graph.
+
+    Parameters
+    ----------
+    G : nx.Graph
+        Graph to draw.
+    pos : dict, optional
+        Dictionary mapping nodes to (x, y) coordinates.
+        If None, spring_layout is used.
+    ax : matplotlib.axes.Axes, optional
+        Axes to draw on.
+    """
+    import matplotlib.pyplot as plt
+    import networkx as nx
+
+    if pos is None:
+        pos = nx.spring_layout(G, seed=0)
+
+    if ax is None:
+        _, ax = plt.subplots(figsize=(8, 8))
+
+    nx.draw_networkx_nodes(
+        G,
+        pos,
+        node_size=node_size,
+        node_color=node_color,
+        ax=ax,
+    )
+
+    nx.draw_networkx_edges(
+        G,
+        pos,
+        edge_color=edge_color,
+        width=2,
+        ax=ax,
+    )
+
+    if with_labels:
+        if labels is None:
+            labels = {n: str(n) for n in G.nodes}
+
+        nx.draw_networkx_labels(
+            G,
+            pos,
+            labels=labels,
+            font_size=10,
+            ax=ax,
+        )
+
+    ax.set_aspect("equal")
+    ax.set_axis_off()
+
+    plt.show()
+
+    return pos
+
+
+# score subgraph
+def score_subgraph(backend, qubits,
+                   w_readout=1.0,
+                   w_1q=1.0,
+                   w_2q=2.0,
+                   w_t1=0.2,
+                   w_t2=0.2
+                   ):
+    """
+    Score a subset of physical qubits.
+
+    Higher score is better.
+
+    Parameters
+    ----------
+    backend : BackendV2
+    qubits : iterable[int]
+        Physical qubit indices.
+    """
+
+    import math
+    import statistics
+
+    target = backend.target
+    qubits = list(qubits)
+    qset = set(qubits)
+
+    readout = []
+    oneq = []
+    t1 = []
+    t2 = []
+    twoq = []
+
+    # ------------------------------
+    # Single-qubit properties
+    # ------------------------------
+
+    for q in qubits:
+
+        props = target.qubit_properties[q]
+
+        if props is not None:
+            if props.t1 is not None:
+                t1.append(props.t1)
+
+            if props.t2 is not None:
+                t2.append(props.t2)
+
+        # Readout error
+        if "measure" in target:
+            inst = target["measure"].get((q,))
+            if inst is not None and inst.error is not None:
+                readout.append(1.0 - inst.error)
+
+        # Best single-qubit gate
+        best = []
+
+        for gate in ("sx", "x", "rz"):
+            if gate in target:
+                inst = target[gate].get((q,))
+                if inst is not None and inst.error is not None:
+                    best.append(1.0 - inst.error)
+
+        if best:
+            oneq.append(max(best))
+
+    # ------------------------------
+    # Two-qubit fidelities
+    # ------------------------------
+
+    for gate in ("cx", "ecr", "cz"):
+
+        if gate not in target:
+            continue
+
+        for edge, inst in target[gate].items():
+
+            if edge[0] in qset and edge[1] in qset:
+
+                if inst.error is not None:
+                    twoq.append(1.0 - inst.error)
+
+    # ------------------------------
+    # Aggregate
+    # ------------------------------
+
+    def mean_or_zero(values):
+        return statistics.mean(values) if values else 0.0
+
+    score = 0.0
+
+    score += w_readout * mean_or_zero(readout)
+    score += w_1q * mean_or_zero(oneq)
+    score += w_2q * mean_or_zero(twoq)
+
+    if t1:
+        score += w_t1 * math.log10(mean_or_zero(t1))
+
+    if t2:
+        score += w_t2 * math.log10(mean_or_zero(t2))
+
+    return score
+
+
+
 class LayoutModelDefault:
     def __init__(self, custom_list=None):
         self._qubits_list = custom_list
@@ -410,13 +582,132 @@ class LayoutModelNapo:
         scores = mm.evaluate_layouts(small_qc, layouts, backend)
         return scores[0][0]
 
-    def plot_data(self, backend, n_qubits):
+    def plot_data(self, backend, n_qubits, circuit):
         from qiskit.visualization import plot_gate_map
         import matplotlib.pyplot as plt
 
         n_backend_qubits = backend.num_qubits
 
-        layout = self.get_layout(None, backend, n_qubits)
+        layout = self.get_layout(circuit, backend, n_qubits)
+
+        if layout is not None:
+            qubit_color = []
+            for i in range(n_backend_qubits):
+                if i in layout:
+                    qubit_color.append("#ff0066")
+                else:
+                    qubit_color.append("#6600cc")
+        else:
+            qubit_color = None
+
+        plot_gate_map(backend, qubit_color=qubit_color, qubit_size=60, font_size=25, figsize=(8, 8))
+
+        plt.show()
+
+
+class LayoutModelGraph:
+    def __init__(self, G_orbital):
+
+        import networkx as nx
+
+        G_spin = nx.Graph()
+
+        # Add alpha-beta pairs
+        for i in G_orbital.nodes:
+            alpha = 2 * i
+            beta = alpha + 1
+
+            G_spin.add_node(alpha)
+            G_spin.add_node(beta)
+
+        # Connect neighbouring orbitals
+        for i, j in G_orbital.edges:
+            alpha_i = 2 * i
+            beta_i = alpha_i + 1
+            alpha_j = 2 * j
+            beta_j = alpha_j + 1
+
+            # print(alpha_i, beta_i, alpha_j)
+
+            G_spin.add_edge(alpha_i, beta_i)
+            G_spin.add_edge(beta_i, alpha_j)
+            G_spin.add_edge(alpha_j, beta_j)
+
+        self._G_circuit = G_spin
+
+        # draw_graph(G_spin)
+
+
+    def get_layout(self, circuit, backend, n_qubits):
+
+        from networkx.algorithms import isomorphism
+        import networkx as nx
+
+        # print(backend.coupling_map)
+        # for i in backend.coupling_map:
+        #    print(i)
+
+        G_backend = nx.Graph()
+        G_backend.add_edges_from(backend.coupling_map)
+
+        # Find one embedding
+        GM = isomorphism.GraphMatcher(G_backend, self._G_circuit)
+        # mapping = next(GM.subgraph_isomorphisms_iter())
+
+        unique_subgraphs = set()
+        for mapping in GM.subgraph_isomorphisms_iter():
+            nodes = frozenset(mapping.keys())  # set of nodes of G
+            unique_subgraphs.add(nodes)
+
+        scores = {}
+        for subgraph in unique_subgraphs:
+            score = score_subgraph(backend, subgraph)
+            scores[subgraph] = score
+
+            # print('score: ', score, ':', list(subgraph))
+
+            plot_map = False
+            if plot_map:
+                import matplotlib.pyplot as plt
+
+                # Edges of the subgraph inside G
+                edges_subgraph = [(u, v) for u, v in G_backend.edges()
+                                  if u in subgraph and v in subgraph]
+
+                # Layout
+                pos = nx.spring_layout(G_backend, seed=42)
+
+                plt.figure()
+
+                # Draw full graph (background)
+                nx.draw(G_backend, pos,
+                        with_labels=True,
+                        node_color='lightgray',
+                        edge_color='lightgray')
+
+                # Highlight nodes of H
+                nx.draw_networkx_nodes(G_backend, pos,
+                                       nodelist=subgraph,
+                                       node_color='red')
+
+                # Highlight edges of H
+                nx.draw_networkx_edges(G_backend, pos,
+                                       edgelist=edges_subgraph,
+                                       edge_color='red',
+                                       width=2)
+
+                plt.title("Subgraph H highlighted inside G")
+                plt.show()
+
+        return max(scores, key=scores.get)
+
+    def plot_data(self, backend, n_qubits, circuit):
+        from qiskit.visualization import plot_gate_map
+        import matplotlib.pyplot as plt
+
+        layout = self.get_layout(circuit, backend, n_qubits)
+
+        n_backend_qubits = backend.num_qubits
 
         if layout is not None:
             qubit_color = []
